2b398dfec06b9200b5d1f7ed2f5a027eb9fedad7
MarcoEfestoPolita <polita.marco@gmail.com>
2017-02-04 18:53:31 +0100

    sequelizer migrations and models

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
+var models = require("./models");
 
 var app = express();
 
---
+++ models/todo.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define('Todo', {
+    text: DataTypes.STRING,
+    chat_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Todo;
+};
